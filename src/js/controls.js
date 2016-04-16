bb774ce2bcf81c35fa1968b7e3d1aed2621c6fee
coreywkruger <coreywkruger@gmail.com>
2016-04-16 11:50:23 -0700

    indentsssss

--- src/js/controls.js
+++ src/js/controls.js
@@ -1,50 +1,50 @@
 function GameControls() {
 
-	var self = this;
-	var actions = {};
-	var keyCodes = {};
-	var active = false;
+  var self = this;
+  var actions = {};
+  var keyCodes = {};
+  var active = false;
 
-	this.createAction = function(key, cb) {
-		console.log('adding', key);
-		actions[key] = cb;
-	};
+  this.createAction = function(key, cb) {
+    console.log('adding', key);
+    actions[key] = cb;
+  };
 
-	this.startControls = function() {
+  this.startControls = function() {
 
-		$(window).on('keydown', function(event) {
-			keyCodes[event.keyCode] = (event.type == 'keydown');
-			active = true;
-		});
+    $(window).on('keydown', function(event) {
+      keyCodes[event.keyCode] = (event.type == 'keydown');
+      active = true;
+    });
 
-		$(window).on('keyup', function(event, a, b) {
-			keyCodes[event.keyCode] = (event.type == 'keydown');
-			if (self._is_empty()) {
-				active = false;
-			}
-		});
+    $(window).on('keyup', function(event, a, b) {
+      keyCodes[event.keyCode] = (event.type == 'keydown');
+      if (self._is_empty()) {
+        active = false;
+      }
+    });
 
-		setInterval(function() {
-			if (active) {
-				self._exec_funs();
-			}
-		}, 1);
-	};
+    setInterval(function() {
+      if (active) {
+        self._exec_funs();
+      }
+    }, 1);
+  };
 
-	this._is_empty = function() {
-		for (var key in keyCodes) {
-			if (keyCodes[key]) return false;
-		}
-		return true;
-	};
+  this._is_empty = function() {
+    for (var key in keyCodes) {
+      if (keyCodes[key]) return false;
+    }
+    return true;
+  };
 
-	this._exec_funs = function() {
-		for (var key in keyCodes) {
-			if (keyCodes[key]) {
-				if (actions[key]) {
-					actions[key]();
-				}
-			}
-		}
-	};
+  this._exec_funs = function() {
+    for (var key in keyCodes) {
+      if (keyCodes[key]) {
+        if (actions[key]) {
+          actions[key]();
+        }
+      }
+    }
+  };
 }
